cbf0a6563e603fd6cdcc8e953dc15f6db9929b4d
Max Weber <weber.max.1988@gmail.com>
2023-05-17 20:52:16 +0200

    [Upgrade] To the newest version of Nextra

---
+++ pages/posts/example.md
@@ -0,0 +1,9 @@
+---
+type: post
+title: Example Post
+date: 2021-03-18
+---
+
+# Example Post
+
+Lorem Ipsum
--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml',
+    title: 'Max Weber - Flutter Explained',
+    site_url: 'https://flutter-explained.dev',
+    feed_url: 'https://flutter-explained/feed.xml',
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
