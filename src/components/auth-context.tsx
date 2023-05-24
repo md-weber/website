373aacfe66446787f73b207f1b3292cecbf326ec
Max Weber <weber.max.1988@gmail.com>
2023-05-24 09:27:08 +0200

    Removed the userId from the auth context

--- src/components/auth-context.tsx
+++ src/components/auth-context.tsx
@@ -4,7 +4,6 @@ import { createContext, useState, ReactNode, useEffect } from 'react';
 
 // Define the shape of our context state
 export interface AuthContextData {
-  userId: string;
   isLoggedIn: boolean;
   setIsLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
   client: SupabaseClient
@@ -20,26 +19,23 @@ export const AuthContext = createContext<AuthContextData | undefined>(undefined)
 export function AuthProvider({ children }: AuthProviderProps) {
   const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_PROJECT_URL, process.env.NEXT_PUBLIC_SUPABASE_API_KEY);
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
-  const [userId, setUserId] = useState<string>("");
   
   useEffect(() => {
     supabase.auth.getSession().then(({data: {session}}) => {
       setIsLoggedIn(Boolean(session))
-      setUserId(session.user.id)
     })
 
     const {
       data: {subscription},
     } = supabase.auth.onAuthStateChange((_event, session) => {
       setIsLoggedIn(Boolean(session))
-      setUserId(session.user.id)
     })
 
     return () => subscription.unsubscribe()
   }, [])
   
   return (
-    <AuthContext.Provider value={{ userId, isLoggedIn, setIsLoggedIn, client: supabase }}>
+    <AuthContext.Provider value={{isLoggedIn, setIsLoggedIn, client: supabase }}>
       {children}
     </AuthContext.Provider>
     );
