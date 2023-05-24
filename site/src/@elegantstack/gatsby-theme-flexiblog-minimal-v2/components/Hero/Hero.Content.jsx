f890997c4a07ae02bea92e47b518818743d15757
Max Weber <weber.max.1988@gmail.com>
2023-05-24 10:43:39 +0200

    Added our service section

--- site/src/@elegantstack/gatsby-theme-flexiblog-minimal-v2/components/Hero/Hero.Content.jsx
+++ site/src/@elegantstack/gatsby-theme-flexiblog-minimal-v2/components/Hero/Hero.Content.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import { Heading } from 'theme-ui'
 import Divider from '@components/Divider'
 
-// site/src/@elegantstack/gatsby-theme-flexiblog-minimal-v2/components/Hero/Hero.Content.jsx
-// site/src/@elegantstack/gatsby-theme-flexiblog-minimal-v2/components/Hero/Hero.Content.jsx
-
 const styles = {
   heading: {
     background: t => `
@@ -29,13 +26,13 @@ const HeroContent = () => (
   <>
     <Divider space={3} />
     <Heading variant='h1' sx={styles.heading}>
-      Learn new skills from
+      Elevate Your Coding Skills 
       <br />
-      Flutter Explained
+      With Flutter Explained
     </Heading>
     <Divider space={3} />
     <Heading variant='h3' sx={styles.running}>
-        We are passionate Flutter Developers who love to share knowledge about Dart and Flutter.
+        Join us on a learning journey and master Flutter development, guided by our passion, expertise, and shared wisdom.
     </Heading>
   </>
 )
--- site/src/@elegantstack/gatsby-theme-flexiblog-minimal-v2/containers/Posts.jsx
+++ site/src/@elegantstack/gatsby-theme-flexiblog-minimal-v2/containers/Posts.jsx
@@ -6,6 +6,15 @@ import Pagination from '@components/Pagination'
 import NewsletterExpanded from '@widgets/NewsletterExpanded'
 import Seo from '@widgets/Seo'
 import HeroComponent from '../../../../../packages/themes/gatsby-theme-flexiblog-minimal-v2/src/components/Hero/Hero'
+import { Heading } from 'theme-ui'
+
+const styles = {
+  heading: {
+    span: {
+      color: `omegaDark`
+    }
+  }
+}
 
 const Posts = ({ data: { paginatedPosts = {}, posts }, ...props }) => {
   const { pageContext: { services = {}, basePath } = {} } = props
@@ -17,11 +26,37 @@ const Posts = ({ data: { paginatedPosts = {}, posts }, ...props }) => {
         <HeroComponent {...props} />
       </Hero>
       <Divider />
+      <Divider />
+      <Divider />
+      <Divider />
+      <Stack>
+        <Main>
+          <Heading variant='h1' sx={styles.heading}>Our Services</Heading>
+          <p>Welcome to Flutter Explained, where we offer a range of services to help you and your team master Flutter development. From training for companies and mentorship programs to public speaking and creating educational content on YouTube, we are here to guide you every step of the way on your Flutter journey.</p>
+
+          <Heading variant='h2' sx={styles.heading}>Professional Training</Heading>
+
+          <p>We offer high-quality, bespoke Flutter training for companies and teams. Our professional training programs are tailored to your specific needs, ensuring that you get the most out of every training session.</p>
+
+          <Heading variant='h2' sx={styles.heading}>Mentorship Program</Heading>
+
+          <p>Join our mentorship program for one-on-one guidance and support. Whether you're new to Flutter or looking to take your skills to the next level, we're here to assist.</p>
+
+          <Heading variant='h2' sx={styles.heading}>Public Speaking</Heading>
+
+          <p>Looking for an experienced, engaging speaker for your event? We have extensive experience speaking at various events and conferences, and we're available for both in-person and virtual engagements.</p>
+
+          <Heading variant='h2' sx={styles.heading}>YouTube Content Creator</Heading>
+
+          <p>Don't miss our educational content on YouTube. We regularly release videos covering a wide range of topics within Flutter development. Subscribe to our channel for regular updates and deepen your knowledge at your own pace.</p>
+        </Main>
+      </Stack>
+      <Divider />
       <Stack>
         <Main sx={{ zIndex: 1 }}>
           <CardList
             variant={['horizontal']}
-            title='What Experts Say About Flutter and Dart'
+            title='Our Blog'
             nodes={paginatedPosts.nodes}
             columns={[1, 2]}
             omitMedia
