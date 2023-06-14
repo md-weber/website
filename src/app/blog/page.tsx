7017b4fa24db112284d4cbcc15b90e9f76d6f217
Max Weber <weber.max.1988@gmail.com>
2023-06-14 09:47:56 +0200

    [Blog] Enable the blog posts

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,10 +1,31 @@
+import * as fs from "fs";
+import Link from "next/link";
+
+const getPostMetaData = () => {
+
+    const folder = "posts/";
+    const files = fs.readdirSync(folder);
+    const markdownPosts = files.filter(file => file.endsWith(".md"));
+    return markdownPosts.map(file => file.replace(".md", ""));
+}
+
 export default function Blog() {
+    const postMetaData = getPostMetaData();
+    const postPreviews = postMetaData.map((slug) => {
+        return (
+            <div key={slug}>
+                <Link href={"/posts/" + slug}>
+                    <h2>{slug}</h2>
+                </Link>
+            </div>)
+    })
     return (
         <div className={"m-auto"}>
             <h1 className={"text-4xl font-semibold text-gray-800 dark:text-white"}>Blog</h1>
-            <section className={"mt-6"}>
+            <section className={"mt-6 dark:text-gray-300"}>
                 <div>
-                    <p>Here is some text</p>
+                    <p>🤟 Welcome to my tech blog 💻</p>
+                    {postPreviews}
                 </div>
             </section>
         </div>
